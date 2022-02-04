7c1bbc981b57d19ac66758c0cfac675673c48d57
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-04 14:11:58 +0530

    Create 1A_Theatre_Square.cpp

---
+++ 1A_Theatre_Square.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/1/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n,m,a;
+    cin>>n>>m>>a;
+    cout<<(((n+a-1)/a)*((m+a-1)/a));
+
+return 0;
+}
