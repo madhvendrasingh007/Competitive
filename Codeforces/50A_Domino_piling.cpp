dfa069ceff6a998ef4b6e691074e6f8356f42695
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-05 18:46:37 +0530

    Create 50A_Domino_piling.cpp

---
+++ Codeforces/50A_Domino_piling.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/50/A
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+
+    cout<<(m*n)/(1*2)<<endl;
+return 0;
+}
