3ac7c661bdcb96d80bf1bd6d97cd4b9abe1e258b
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-08 22:30:55 +0530

    Add files via upload

---
+++ Codeforces/732A_Buy_a_Shovel.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/732/A
+#include<iostream>
+using namespace std;
+
+int main(){
+    int k, r, h = 1, i = 0, j = 1;
+    bool b = true;
+    cin>>k>>r;
+
+    while (b) {
+        i++;
+        h = k * i;
+        if (h % 10 == 0 || h % 10 == r) {
+            b = false;
+        }
+    }
+    cout << i;
+
+
+return 0;
+}
