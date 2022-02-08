8d75dbc7900f5c3aad4b5864c6b1d220da384fcb
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-08 22:25:59 +0530

    Create 1360B_Hones_Coach.cpp

---
+++ Codeforces/1360B_Hones_Coach.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/problemset/problem/1360/B
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+    int t,n,a[100];
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        sort(a,a+n);
+        vector<int>v;
+        int dif = 0;
+        for(int i=1; i<n; i++){
+            dif = a[i] - a[i-1];
+            v.push_back(dif);
+        }
+
+        sort(v.begin(),v.end());
+        cout<<v[0]<<endl;
+    }
+    return 0;
+
+}
