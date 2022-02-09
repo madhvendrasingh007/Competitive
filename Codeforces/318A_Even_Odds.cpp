15efd4ccf597648d7a6c9194ca7a38adc0aba32d
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-09 21:19:17 +0530

    Add files via upload

---
+++ Codeforces/318A_Even_Odds.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll n, k;
+    cin>>n >>k;
+    
+    if (k <= (n+1)/2)
+        cout << k*2-1 << endl;
+    
+    else
+        cout << (k - (n+1)/2)*2 << endl;
+    
+    return 0;
+}
