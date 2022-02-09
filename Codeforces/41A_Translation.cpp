e74c69fe6ea4fa0188700f0bab7fed741c267895
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-09 21:15:08 +0530

    Add files via upload

---
+++ Codeforces/41A_Translation.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/41/A
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+    if (s == t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
