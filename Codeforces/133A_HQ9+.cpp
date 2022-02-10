62535f722edf6d12a81312535b613ff4bf781340
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-10 22:35:39 +0530

    Add files via upload

---
+++ Codeforces/133A_HQ9+.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/133/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+	cin>>str;
+    
+	int flag=0;
+
+	for(int i=0;i<str.size();i++){
+	    if(str[i]>=33&&str[i]<=126){
+	        if(str[i]=='H'||str[i]=='Q'||str[i]=='9')
+	            flag=1;
+	}
+	}
+	if(flag){
+	    cout<<"YES";
+	}else{
+	    cout<<"NO";
+	}
+return 0;
+}
