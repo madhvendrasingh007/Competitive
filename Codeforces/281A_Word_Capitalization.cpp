a5048bb673f3bb0328bcde6e31a4a2282bb84f8d
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-10 22:33:20 +0530

    Add files via upload

---
+++ Codeforces/281A_Word_Capitalization.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/281/A
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    s[0] = towupper(s[0]);
+    cout << s << endl;
+    return 0;
+
+return 0;
+}
