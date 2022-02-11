0607522ea7dbd21abe2518b85051ef11cd81c5bc
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-11 21:16:50 +0530

    Add files via upload

---
+++ Codeforces/136A_Presents.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/136/A
+
+#include<iostream>
+using namespace std;
+int n, b, mas[105];
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> b;
+        mas[b] = i;
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << mas[i] << " ";
+    }
+}
